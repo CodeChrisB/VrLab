51cb7c6bbe84162580627246a33fa53edeacba67
Chris <c.buchberger01@gmail.com>
2021-09-04 17:03:13 +0200

    Teleport System + Input Wrapper

---
+++ Assets/Scripts/Movement/XrEnum.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum XrEnum 
+{
+   //Left Controller
+   LeftPosition,
+   LeftRotation,
+   LeftSelect,
+   LeftActivate,
+   LeftUiPress,
+   LeftHapticDevice,
+   LeftTeleportSelect,
+   LeftTeleportModeActivate,
+   LeftTeleportModeCancel,
+   LeftTurn,
+   LeftMove,
+   LeftRotateAnchor,
+   LeftTranslateAnchor,
+   //Right Controller
+   RightPosition,
+   RightRotation,
+   RightSelect,
+   RightActivate,
+   RightUiPress,
+   RightHapticDevice,
+   RightTeleportSelect,
+   RightTeleportModeActivate,
+   RightTeleportModeCancel,
+   RightTurn,
+   RightMove,
+   RightRotateAnchor,
+   RightTranslateAnchor,
+   //Head Controller
+   HeadPosition,
+   HeadRotation
+}
